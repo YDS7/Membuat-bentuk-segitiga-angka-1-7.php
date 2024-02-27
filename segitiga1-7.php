b4808a68ea1d27b7c877f2744670c27563b378bd
YDS7 <127652220+YDS7@users.noreply.github.com>
2024-02-27 15:08:54 +0700

    php
    bahasa pemrograman PHP

---
+++ segitiga1-7.php
@@ -0,0 +1,11 @@
+<?php
+for ($y=1; $y <=7 ; $y++) {
+    for ($n=6; $n>=$y; $n--){
+        echo"&nbsp;&nbsp;";    
+    }
+    for ($a=1; $a <=$y ; $a++) {
+        echo"$a";
+    }
+    echo"<br>";
+}
+?>
